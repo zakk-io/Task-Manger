eeac9a041f55db1f4df5e8ed0ae1adba047c4d40
zakk-io <mozakk.io@gmail.com>
2024-11-19 18:33:10 +0200

    list tasks client side

--- public/browser-app.js
+++ public/browser-app.js
@@ -1,3 +1,40 @@
+//list tasks
+const taskscontainer = document.getElementById("tasks-container")
+
+const ListTasks = async function(){
+  const response = await fetch('/api/tasks',{
+    method : "GET",
+  })
+
+  const data = await response.json()
+  if(data.successful === true){
+    var Tasks = ""
+    for (let task = data.tasks.length - 1; task >= 0; task--) {
+        const name = data.tasks[task].name
+        const completed = data.tasks[task].complete
+
+        Tasks += `<div class="single-task ${completed}">
+                  <h5><span><i class="far fa-check-circle"></i></span>${name}</h5>
+                  <div class="task-links">
+                  
+                  <!-- edit link -->
+                  <a href="task.html?id="  class="edit-link">
+                  <i class="fas fa-edit"></i>
+                  </a>
+                  <!-- delete btn -->
+                  <button type="button" class="delete-btn" data-id="">
+                  <i class="fas fa-trash"></i>
+                  </button>
+                  </div>
+                  </div>`
+        
+    }
+    taskscontainer.innerHTML = Tasks
+  }
+
+}
+//list tasks
+
 //create task
 const taskform = document.getElementById("task-form")
 const taskinput = document.getElementById("task-input")
@@ -22,10 +59,14 @@ taskform.addEventListener('submit',async (e) => {
     formalert.innerHTML = data.body
   }
 
-  else if(data.status === 200){
+  else if(data.status === 201){
+    taskinput.value = ""
     formalert.innerHTML = data.message
+    ListTasks()
   }
 })
-
-
 //create task
+
+
+
+ListTasks()
